93bdb9b1828a104cfdf9ea4b7e2fe98172850305
dkloz <jimkotz2@gmail.com>
2018-05-12 15:01:54 -0700

    ads

--- util/data_io.py
+++ util/data_io.py
@@ -21,8 +21,6 @@ def load_test(dataset='12m'):
 
 def get_word_embeddings(dataset='9sr'):
     """Returns 300d embedding matrix based on 50k vocab from first_all."""
-    if '10sr' in dataset or 'sr10' in dataset or 'sr9' in dataset or '9sr' in dataset or '200sr' in dataset:
-        dataset = '9sr'
     filename = os.path.join(word_embeddings_dir, 'query_completion', '%s_glove_matrix_300d.pkl' % dataset)
     if os.path.exists(filename):
         return load_pickle(filename, False)
