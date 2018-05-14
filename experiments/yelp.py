fcb7248e5dac4dcb6135e27b6a4654dff593b4ba
Dimitrios Kotzias-imac <jimkotz2@gmail.com>
2018-05-13 17:27:59 -0700

    exp

---
+++ experiments/yelp.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+from experiments.exp_helper import make_data_train_model
+from experiments.exp_helper import read_gpu_lr
+from experiments.test import test_model
+
+gpu, _ = read_gpu_lr(False)
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+dataset_name = 'yelp'
+
+#
+context = 'user'
+
+exp_name = dataset_name + '_' + context + '_query_10000.txt'
+sys.stdout = open(exp_name, 'w')
+
+testdata = make_data_train_model(dataset_name, context)
+test_model(dataset_name, context, testdata)
+sys.stdout.flush()
+
+context = 'category'
+
+exp_name = dataset_name + '_' + context + '_query_10000.txt'
+sys.stdout = open(exp_name, 'w')
+
+testdata = make_data_train_model(dataset_name, context)
+test_model(dataset_name, context, testdata)
+sys.stdout.flush()
